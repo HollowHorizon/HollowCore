207a654ee61da6205e42e1ce2754b4431d819937
HollowHorizon <artemm@xaker.ru>
2024-02-09 18:25:10 +0300

    ui animations (wip)

--- src/main/java/ru/hollowhorizon/hc/common/ui/Widget.kt
+++ src/main/java/ru/hollowhorizon/hc/common/ui/Widget.kt
@@ -3,7 +3,6 @@ package ru.hollowhorizon.hc.common.ui
 import com.mojang.blaze3d.vertex.PoseStack
 import kotlinx.serialization.Serializable
 import net.minecraft.client.Minecraft
-import net.minecraft.client.gui.screens.Screen
 import net.minecraft.world.entity.player.Player
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
@@ -15,13 +14,15 @@ import ru.hollowhorizon.hc.client.screens.UIScreen
 import ru.hollowhorizon.hc.client.utils.ScissorUtil
 import ru.hollowhorizon.hc.client.utils.nbt.NBTFormat
 import ru.hollowhorizon.hc.client.utils.nbt.serialize
+import ru.hollowhorizon.hc.client.utils.use
 import ru.hollowhorizon.hc.common.network.HollowPacketV2
 import ru.hollowhorizon.hc.common.network.HollowPacketV3
-import ru.hollowhorizon.hc.common.ui.widgets.ButtonWidget
+import ru.hollowhorizon.hc.common.ui.animations.AnimationTrigger
+import ru.hollowhorizon.hc.common.ui.animations.UIAnimation
 import ru.hollowhorizon.hc.common.ui.widgets.button
 import ru.hollowhorizon.hc.common.ui.widgets.entity
 import ru.hollowhorizon.hc.common.ui.widgets.image
-import java.util.*
+import kotlin.collections.ArrayList
 
 fun gui(builder: Widget.() -> Unit) = Widget().apply(builder)
 fun Widget.gui(builder: Widget.() -> Unit) { this += Widget().apply(builder) }
@@ -74,12 +75,14 @@ interface IWidget {
 @Serializable
 @Polymorphic(IWidget::class)
 open class Widget : IWidget {
-    private val widgets = ArrayList<IWidget>()
+    internal val widgets = ArrayList<IWidget>()
+    internal val animations = ArrayList<UIAnimation>()
     var width: ScreenPosition = 200.px
     var height: ScreenPosition = 100.px
     var padding = Padding(0.px, 0.px, 0.px, 0.px)
     var alignment = Alignment.CENTER
     var enableScissors = false
+    var zLayer = 0
 
     @OnlyIn(Dist.CLIENT)
     override fun render(
@@ -88,7 +91,9 @@ open class Widget : IWidget {
         screenWidth: Int, screenHeight: Int,
         widgetWidth: Int, widgetHeight: Int,
         mouseX: Int, mouseY: Int, partialTick: Float,
-    ) {
+    ) = stack.use {
+        translate(0.0, 0.0, zLayer.toDouble())
+
         val width = width(screenWidth, screenHeight, widgetWidth, widgetHeight, mouseX, mouseY)
         val height = height(screenWidth, screenHeight, widgetWidth, widgetHeight, mouseX, mouseY)
         val nx = (x + widgetWidth * alignment.factorX - width * alignment.factorX).toInt()
@@ -163,6 +168,10 @@ open class Widget : IWidget {
             mouseX,
             mouseY
         )
+    }
+
+    fun animations(vararg animations: UIAnimation) {
+        this.animations.addAll(animations)
     }
 
     fun size(x: ScreenPosition, y: ScreenPosition) {
---
+++ src/main/java/ru/hollowhorizon/hc/common/ui/animations/UIAnimation.kt
@@ -0,0 +1,44 @@
+package ru.hollowhorizon.hc.common.ui.animations
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import ru.hollowhorizon.hc.client.handlers.ClientTickHandler
+import ru.hollowhorizon.hc.client.utils.math.Interpolation
+
+@Serializable
+class UIAnimation(
+    vararg val targets: AnimationTarget,
+    val trigger: AnimationTrigger,
+    val duration: Int = 20,
+    val startValue: Float = 0f,
+    val endValue: Float = 1f,
+    val interpolation: Interpolation = Interpolation.LINEAR,
+) {
+    @Transient
+    var startTime = ClientTickHandler.ticks
+    val isEnd get() = trigger != AnimationTrigger.LOOP || ClientTickHandler.ticks - startTime > duration
+
+    fun reset() {
+        startTime = ClientTickHandler.ticks
+    }
+
+    fun update(partialTick: Float): Float {
+        val time = (ClientTickHandler.ticks - startTime + partialTick % duration) / duration
+
+        return startValue + (endValue - startTime) * interpolation(time)
+    }
+}
+
+enum class AnimationTarget {
+    OFFSET_X, OFFSET_Y,
+    SCALE_X, SCALE_Y,
+    ROTATION,
+    COLOR_R, COLOR_G, COLOR_B,
+    TRANSPARENCY, CUSTOM;
+
+    companion object
+}
+
+enum class AnimationTrigger {
+    ON_OPEN, ON_CLOSE, ON_CLICK, ON_HOVER, ON_UNHOVER, LOOP, CUSTOM;
+}
