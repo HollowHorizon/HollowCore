d16853e0f74dd2195b8be2e0d6bbdeb62a70545a
HollowHorizon <artemm@xaker.ru>
2024-09-24 10:25:01 +0300

    Multiblocks Tag Support

--- src/main/java/ru/hollowhorizon/hc/common/multiblock/Multiblock.kt
+++ src/main/java/ru/hollowhorizon/hc/common/multiblock/Multiblock.kt
@@ -1,24 +1,30 @@
 package ru.hollowhorizon.hc.common.multiblock
 
-import net.minecraft.core.BlockPos
-import net.minecraft.core.Direction
 //? if >=1.20.1 {
 /*import net.minecraft.core.registries.Registries
-import ru.hollowhorizon.hc.client.utils.registryAccess
+import net.minecraft.core.registries.BuiltInRegistries
 *///?} else {
 import net.minecraft.data.BuiltinRegistries
+import net.minecraft.core.Registry
 //?}
+
+import ru.hollowhorizon.hc.client.utils.registryAccess
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.tags.TagKey
 import net.minecraft.world.level.BlockAndTintGetter
 import net.minecraft.world.level.ColorResolver
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.LightLayer
 import net.minecraft.world.level.biome.Biomes
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.lighting.LevelLightEngine
 import net.minecraft.world.level.material.FluidState
+import ru.hollowhorizon.hc.client.utils.registryAccess
 
 
 class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
@@ -26,7 +32,7 @@ class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
     var xSize: Int = 0
     var ySize: Int = 0
     var zSize: Int = 0
-    val blocks = ArrayList<BlockState>()
+    val blocks = ArrayList<Matcher>()
 
     init {
         block()
@@ -38,11 +44,11 @@ class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
         this.ySize = ySize
     }
 
-    fun pattern(vararg blocks: BlockState?) {
+    fun pattern(vararg blocks: Matcher?) {
         assert(blocks.size != xSize * ySize * zSize) { "Blocks must have the same size" }
 
         this.blocks.clear()
-        this.blocks.addAll(blocks.map { it ?: Blocks.AIR.defaultBlockState() })
+        this.blocks.addAll(blocks.map { it ?: block(Blocks.AIR.defaultBlockState()) })
     }
 
     fun isValid(level: Level, basePos: BlockPos) =
@@ -76,13 +82,13 @@ class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
             for (z in 0..<zSize) {
                 for (x in 0..<xSize) {
                     val expectedBlock = blocks[x + z * zSize + y * zSize * xSize]
-                    if (expectedBlock.isAir) continue
+                    if (expectedBlock.default().isAir) continue
 
                     // Смещаем блоки относительно выбранной стартовой точки
                     val rotatedPos = getRotatedPos(basePos, x - startX, y - startY, z - startZ, direction)
                     val currentBlock = level.getBlockState(rotatedPos)
 
-                    if (currentBlock != expectedBlock) return false
+                    if (!expectedBlock.matches(currentBlock)) return false
                 }
             }
         }
@@ -110,7 +116,7 @@ class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
     override fun getBrightness(type: LightLayer, pos: BlockPos) = 14
 
 
-    override fun getRawBrightness(pos: BlockPos, ambientDarkening: Int) = 15-ambientDarkening
+    override fun getRawBrightness(pos: BlockPos, ambientDarkening: Int) = 15 - ambientDarkening
 
 
     override fun getBlockEntity(pos: BlockPos): BlockEntity? {
@@ -129,7 +135,7 @@ class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
     override fun getBlockState(pos: BlockPos): BlockState {
         val id = pos.x + pos.z * zSize + pos.y * zSize * xSize
         if (id !in 0..<blocks.size) return Blocks.LIGHT.defaultBlockState()
-        return blocks[id]
+        return blocks[id].default()
     }
 
     override fun getFluidState(pos: BlockPos): FluidState = getBlockState(pos).fluidState
@@ -144,10 +150,41 @@ class Multiblock(block: Multiblock.() -> Unit) : BlockAndTintGetter {
 
     override fun getBlockTint(pos: BlockPos, color: ColorResolver): Int {
         //? if >=1.20.1 {
-        /*return color.getColor(registryAccess.registry(Registries.BIOME).get().getOrThrow(Biomes.PLAINS), pos.x.toDouble(), pos.z.toDouble())
+        /*return color.getColor(
+            registryAccess.registry(Registries.BIOME).get().getOrThrow(Biomes.PLAINS),
+            pos.x.toDouble(),
+            pos.z.toDouble()
+        )
         *///?} else {
         return color.getColor(BuiltinRegistries.BIOME.getOrThrow(Biomes.PLAINS), pos.x.toDouble(), pos.z.toDouble())
         //?}
     }
 
+    interface Matcher {
+        fun matches(block: BlockState): Boolean
+
+        fun default(): BlockState
+    }
+
+    fun block(state: BlockState) = object : Matcher {
+        override fun matches(block: BlockState): Boolean {
+            return block == state
+        }
+
+        override fun default() = state
+    }
+
+    fun tag(tag: TagKey<Block>) = object : Matcher {
+        override fun matches(block: BlockState) = block.`is`(tag)
+
+        override fun default(): BlockState {
+            //? if >=1.20.1 {
+            /*return BuiltInRegistries.BLOCK
+            *///?} else {
+            return registryAccess.registry(Registry.BLOCK_REGISTRY).get()
+            //?}
+                .getTag(tag).get().firstOrNull()?.value()
+                ?.defaultBlockState() ?: Blocks.AIR.defaultBlockState()
+        }
+    }
 }
