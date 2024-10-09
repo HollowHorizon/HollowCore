94ca54670fda1bd281f8b5543c61b37822c64155
HollowHorizon <artemm@xaker.ru>
2024-10-09 14:19:22 +0300

    1.21 fix

--- src/main/java/ru/hollowhorizon/hc/common/registry/ModItems.kt
+++ src/main/java/ru/hollowhorizon/hc/common/registry/ModItems.kt
@@ -1,16 +1,5 @@
 package ru.hollowhorizon.hc.common.registry
 
-import net.minecraft.world.item.Item
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.material.Material
-import ru.hollowhorizon.hc.common.objects.blocks.BlockItemProperties
-
 object ModItems : HollowRegistry() {
-    val TEST by register("example", autoModel = AutoModelType.CUBE_ALL) {
-        ExampleBlock()
-    }
 }
 
-class ExampleBlock: Block(Properties.of(Material.METAL)), BlockItemProperties {
-    override val properties = Item.Properties()
-}
