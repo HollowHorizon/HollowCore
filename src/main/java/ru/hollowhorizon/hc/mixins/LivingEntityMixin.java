405d8f97266a56473eddca35f83e15d0612bc7ef
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2025-02-26 23:45:23 +0700

    Living death event

--- src/main/java/ru/hollowhorizon/hc/mixins/LivingEntityMixin.java
+++ src/main/java/ru/hollowhorizon/hc/mixins/LivingEntityMixin.java
@@ -1,5 +1,6 @@
 package ru.hollowhorizon.hc.mixins;
 
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ru.hollowhorizon.hc.client.utils.JavaHacks;
 import ru.hollowhorizon.hc.common.events.EventBus;
+import ru.hollowhorizon.hc.common.events.entity.LivingEntityDeathEvent;
 import ru.hollowhorizon.hc.common.events.tick.TickEvent;
 
 @Mixin(LivingEntity.class)
@@ -14,5 +16,12 @@ public class LivingEntityMixin {
     @Inject(method = "tick", at = @At("TAIL"))
     private void tick(CallbackInfo ci) {
         EventBus.post(new TickEvent.Entity(JavaHacks.forceCast(this)));
+    }
+
+    @Inject(method = "die", at = @At("HEAD"), cancellable = true)
+    public void die(DamageSource damageSource, CallbackInfo ci) {
+        var e = new LivingEntityDeathEvent(JavaHacks.forceCast(this), damageSource);
+        EventBus.post(e);
+        if (e.isCanceled()) ci.cancel();
     }
 }
