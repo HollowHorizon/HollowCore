58d0f1faf53ca9648044c2bfc168c3e226341f52
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2025-03-06 22:40:22 +0700

    Remove recipe condition implementation

--- src/main/java/ru/hollowhorizon/hc/mixins/RecipeSerializerMixin.java
+++
@@ -1,9 +0,0 @@
-package ru.hollowhorizon.hc.mixins;
-
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import org.spongepowered.asm.mixin.Mixin;
-import ru.hollowhorizon.hc.common.objects.recipe.deep.HollowRecipeSerializer;
-
-@Mixin(RecipeSerializer.class)
-interface RecipeSerializerMixin<T extends Recipe<?>> extends HollowRecipeSerializer<T> {}
